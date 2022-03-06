6e7b158f2f5e8d1283228de95cb6496cd7e8f745
BishamontenSama <62648846+hrutugandha@users.noreply.github.com>
2022-03-06 22:16:42 +0530

    completed

--- index.js
+++ index.js
@@ -3,54 +3,53 @@ const express = require("express");
 
 const app = express();
 
-app.get("/books",allBooks,(req, res) => {
+app.get("/books", allBooks, (req, res) => {
+  return res.send({
+    route: "/books",
+  });
+});
 
-    return res.send({
-        "route":"/books",
-    })
-})
+app.use(singleBook);
 
-app.use(singleBook)
+app.get("/books/theSecret", (req, res) => {
+  return res.send({
+    "bookName": req.name,
+     route: "/books/theSecret",
+  });
+});
 
-app.get("/theSecret",(req, res) => {
+app.get("/books/theAlchemist",(req, res) => {
+  return res.send({
+    "bookName": req.name,
+    "route": "/books/theAlchemist",
+  });
+});
 
-    return res.send({
-        route: "/theSecret",
-    })
-})
-
-
-app.get("/theAlchemist",(req, res) => {
-
-    return res.send({
-        route: "/theSecret",
-    })
-})
-
-
-app.get("/theSecret",(req, res) => {
-
-    return res.send({
-        route: "/theSecret",
-    })
-})
+app.get("/books/harryPotter",(req, res) =>
+  res.send({
+    "bookName": req.name,
+    "route": "/books/harryPotter",
+  })
+);
 
 function allBooks(req, res, next) {
+  console.log("fetching all books...");
 
-    console.log("fetching all books...")
-
-    next();
+  next();
 }
 
-function singleBook(req, res, next){
-    if(req.path === "theSecret"){
-        req.name = req.params.name ;
-    }
-    next();
+function singleBook(req, res, next) {
+  if (req.path === "/books/theSecret") {
+    req.name = req.params.name;
+  } else if (req.path === "/books/theAlchemist") {
+    req.name = req.params.name;
+  } else if (req.path === "/books/harryPotter") {
+    req.name = req.params.name;
+  }
+  next();
 }
-
 
 app.listen(5000, () => {
-    console.log("Listening on the port 5000")
-})
+  console.log("Listening on the port 5000");
+});
 
